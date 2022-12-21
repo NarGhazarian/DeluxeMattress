8501670c6766a729775f1148e488a81ea4415aa9
Narek <narekghazarjan@gmail.com>
2022-12-21 16:39:00 +0400

    First Commit

---
+++ src/main/java/com/example/deluxemattress/DeluxeMattressApplication.java
@@ -0,0 +1,13 @@
+package com.example.deluxemattress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeluxeMattressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeluxeMattressApplication.class, args);
+    }
+
+}
